0afa1104bd956ab4832eae48bea08d0d7098386b
JG <ludwing.gomez@epn.edu.ec>
2022-10-31 12:27:53 -0500

    select ActoNotarial

--- src/main/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/Modelo/ActoNotarial.java
+++ src/main/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/Modelo/ActoNotarial.java
@@ -1,54 +1,78 @@
 package ec.gob.cj.pesnot.paginaprincipal.catalogoservicios.Modelo;
 
-import java.math.BigDecimal;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 @Entity
-@Table(name = "acto_notarial")
+@Table(name = "CATALOGOACTONOTARIAL")
 public class ActoNotarial {
 	
 	@Id
-	@Column(name="id")
+	@Column(name="IDACTONOTARIAL")
 	private Long id;
 	
-	@Column(name = "nombre_acto_notarial")
+	//TODO Forein key de base cobro 
+	@Column(name = "IDBASECOBRO")
+	private Long baseCobro;
+
+	@ManyToOne(targetEntity = Libro.class, fetch = FetchType.EAGER)
+	@JoinColumn(name = "IDTIPOLIBRO",nullable=false)
+	@JsonProperty(access = Access.READ_WRITE)
+	private Libro libroActoNotarial;
+	
+	@Column(name = "NOMBRECATALOGOACTONOTARIAL")
 	private String nombreActoNotarial;
 
-	@Column(name = "libro_acto_notarial")
-	private String libroActoNotarial;
-
-	@Column(name = "estado_acto_notarial")
-	private String estadoActoNotarial;
+	@Column(name = "ARTICULOCATALOGOACTONOTARIAL")
+	private String articuloActoNotarial;
 	
-	@Column(name = "tarifa")
-	private BigDecimal tarifa;
+	@Column(name = "GRAVALIVACATALOGOACTONOTARIAL")
+	private Boolean grabalIva;
+	
+	@Column(name = "VALOREXONERACIONCATALOGOACTONOTARIAL") 	
+	private Long valorExoneracion;
+	
+	@Column(name = "VALOREXTRACATALOGOACTONOTARIAL") 	
+	private Long valorExtra;
 
-	public BigDecimal getTarifa() {
-		return tarifa;
-	}
+	@Column(name = "HABILITANTECIUDADANOACTONOTARIAL")
+	private Boolean habilitanteCiudadano;
+	
+	@Column(name = "TRAMITEUNILATERALACTONOTARIAL")
+	private Boolean tramiteUnilateralCiudadano;
 
-	public void setTarifa(BigDecimal tarifa) {
-		this.tarifa = tarifa;
-	}
+	@Column(name = "ESTADOACTONOTARIAL")
+	private Boolean estado;
 
-
-
-	public ActoNotarial(Long id, String nombreActoNotarial, String libroActoNotarial, String estadoActoNotarial,
-			BigDecimal tarifa) {
-		super();
-		this.id = id;
-		this.nombreActoNotarial = nombreActoNotarial;
-		this.libroActoNotarial = libroActoNotarial;
-		this.estadoActoNotarial = estadoActoNotarial;
-		this.tarifa = tarifa;
-	}
-
+	
+	
 	public ActoNotarial() {
 		super();
+	}
+	
+	public ActoNotarial(Long id, Long baseCobro, Libro libroActoNotarial, String nombreActoNotarial,
+			String articuloActoNotarial, Boolean grabalIva, Long valorExoneracion, Long valorExtra,
+			Boolean habilitanteCiudadano, Boolean tramiteUnilateralCiudadano, Boolean estado) {
+		super();
+		this.id = id;
+		this.baseCobro = baseCobro;
+		this.libroActoNotarial = libroActoNotarial;
+		this.nombreActoNotarial = nombreActoNotarial;
+		this.articuloActoNotarial = articuloActoNotarial;
+		this.grabalIva = grabalIva;
+		this.valorExoneracion = valorExoneracion;
+		this.valorExtra = valorExtra;
+		this.habilitanteCiudadano = habilitanteCiudadano;
+		this.tramiteUnilateralCiudadano = tramiteUnilateralCiudadano;
+		this.estado = estado;
 	}
 
 	public Long getId() {
@@ -59,6 +83,22 @@ public class ActoNotarial {
 		this.id = id;
 	}
 
+	public Long getBaseCobro() {
+		return baseCobro;
+	}
+
+	public void setBaseCobro(Long baseCobro) {
+		this.baseCobro = baseCobro;
+	}
+
+	public Libro getLibroActoNotarial() {
+		return libroActoNotarial;
+	}
+
+	public void setLibroActoNotarial(Libro libroActoNotarial) {
+		this.libroActoNotarial = libroActoNotarial;
+	}
+
 	public String getNombreActoNotarial() {
 		return nombreActoNotarial;
 	}
@@ -67,19 +107,70 @@ public class ActoNotarial {
 		this.nombreActoNotarial = nombreActoNotarial;
 	}
 
-	public String getLibroActoNotarial() {
-		return libroActoNotarial;
+	public String getArticuloActoNotarial() {
+		return articuloActoNotarial;
 	}
 
-	public void setLibroActoNotarial(String libroActoNotarial) {
-		this.libroActoNotarial = libroActoNotarial;
+	public void setArticuloActoNotarial(String articuloActoNotarial) {
+		this.articuloActoNotarial = articuloActoNotarial;
 	}
 
-	public String getEstadoActoNotarial() {
-		return estadoActoNotarial;
+	public Boolean getGrabalIva() {
+		return grabalIva;
 	}
 
-	public void setEstadoActoNotarial(String estadoActoNotarial) {
-		this.estadoActoNotarial = estadoActoNotarial;
+	public void setGrabalIva(Boolean grabalIva) {
+		this.grabalIva = grabalIva;
 	}
+
+	public Long getValorExoneracion() {
+		return valorExoneracion;
+	}
+
+	public void setValorExoneracion(Long valorExoneracion) {
+		this.valorExoneracion = valorExoneracion;
+	}
+
+	public Long getValorExtra() {
+		return valorExtra;
+	}
+
+	public void setValorExtra(Long valorExtra) {
+		this.valorExtra = valorExtra;
+	}
+
+	public Boolean getHabilitanteCiudadano() {
+		return habilitanteCiudadano;
+	}
+
+	public void setHabilitanteCiudadano(Boolean habilitanteCiudadano) {
+		this.habilitanteCiudadano = habilitanteCiudadano;
+	}
+
+	public Boolean getTramiteUnilateralCiudadano() {
+		return tramiteUnilateralCiudadano;
+	}
+
+	public void setTramiteUnilateralCiudadano(Boolean tramiteUnilateralCiudadano) {
+		this.tramiteUnilateralCiudadano = tramiteUnilateralCiudadano;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "ActoNotarial [id=" + id + ", baseCobro=" + baseCobro + ", libroActoNotarial=" + libroActoNotarial
+				+ ", nombreActoNotarial=" + nombreActoNotarial + ", articuloActoNotarial=" + articuloActoNotarial
+				+ ", grabalIva=" + grabalIva + ", valorExoneracion=" + valorExoneracion + ", valorExtra=" + valorExtra
+				+ ", habilitanteCiudadano=" + habilitanteCiudadano + ", tramiteUnilateralCiudadano="
+				+ tramiteUnilateralCiudadano + ", estado=" + estado + "]";
+	}	
+	
+
 }
--- src/main/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/controlador/ActoNotarialControlador.java
+++ src/main/java/ec/gob/cj/pesnot/paginaprincipal/catalogoservicios/controlador/ActoNotarialControlador.java
@@ -7,99 +7,70 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import ec.gob.cj.pesnot.paginaprincipal.catalogoservicios.Modelo.ActoNotarial;
 import ec.gob.cj.pesnot.paginaprincipal.catalogoservicios.Modelo.RequisitoActoNotarial;
 import ec.gob.cj.pesnot.paginaprincipal.catalogoservicios.service.ActoNotarialService;
 import ec.gob.cj.pesnot.paginaprincipal.catalogoservicios.service.RequisitoActoNotarialService;
 
 @RestController
-@RequestMapping("api/v1")
-@CrossOrigin(originPatterns = "http://localhost:4200")
+@RequestMapping("ActoNotarial")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ActoNotarialControlador {
-	
+
 	@Autowired
-	private ActoNotarialService  actoNotarialService;
+	private ActoNotarialService actoNotarialService;
 	@Autowired
 	private RequisitoActoNotarialService requisitoActoNotarialService;
-	
-	
-	
+
 	public ActoNotarialControlador(ActoNotarialService actoNotarialService,
 			RequisitoActoNotarialService requisitoActoNotarialService) {
 		super();
 		this.actoNotarialService = actoNotarialService;
 		this.requisitoActoNotarialService = requisitoActoNotarialService;
 	}
-	
-	
-	
-	@RequestMapping(path = "/actos", method= RequestMethod.GET)
-	public List<ActoNotarial> getActosNotariales()
-	{
-		
-		List<ActoNotarial> ListaActosNotariales=actoNotarialService.getActosNotariales();
+
+	@GetMapping("/getActos")
+	public List<ActoNotarial> getActosNotariales() {
+
+		List<ActoNotarial> ListaActosNotariales = actoNotarialService.getActosNotariales();
 		return ListaActosNotariales;
-		
+
 	}
 	
-	
-	
-	
-	@GetMapping(path = "requisitos/{id}")
-	public List<RequisitoActoNotarial> ListaRequisitosActos(@PathVariable Long id)
-	{
+	//TODO es por la tbala de requisitos que no se que hace  
+	@GetMapping("/requisitos/{id}")
+	public List<RequisitoActoNotarial> ListaRequisitosActos(@PathVariable("id") Long id) {
 
 		return requisitoActoNotarialService.getRequisitosActosNotarialesId(id);
 
 	}
-	
-	
-	
-	
-@GetMapping (path ="tarifa/{id}")
-public ActoNotarial  getActoNotarialTarifa(@PathVariable Long id)
-{
 
-return actoNotarialService.getActoNotarialTarifa(id);
+	@GetMapping("getActoById/{id}")
+	public ActoNotarial getActoNotarialTarifa(@PathVariable("id") Long id) {
 
-}
+		return actoNotarialService.getActoNotarialTarifa(id);
 
+	}
+	//TODO no esta implementado nada en el repositorio
+	@GetMapping("getActoByNombre/{nombre}")
+	public List<ActoNotarial> getActoNotarialNombre(@PathVariable("nombre") String nombre) {
 
+		return actoNotarialService.getActoNotarialNombre(nombre);
 
-@GetMapping(path = "actosNotariales/{nombre}")
-public List<ActoNotarial> getActoNotarialNombre(@PathVariable String nombre){
-	
-	
-	return actoNotarialService.getActoNotarialNombre(nombre);
-	
-}
+	}
 
+	@GetMapping("getActosLike/{nombre}")
+	public List<ActoNotarial> getActoLik(@PathVariable("nombre") String nombre) {
 
+		return actoNotarialService.getActosLike(nombre);
 
-@GetMapping(path = "actosLike/{nombre}")
-public List<ActoNotarial> getActoLik(@PathVariable String nombre) {
-	
-	return actoNotarialService.getActosLike(nombre);
+	}
 
+	@GetMapping("geActoByLibros/{libro}")
+	public List<ActoNotarial> getActoLibro(@PathVariable("libro") String libro) {
+		return actoNotarialService.getActosLibros(libro);
 
-}
-
-
-
-@GetMapping(path = "libros/{libro}")
-public List<ActoNotarial> getActoLibro(@PathVariable String libro)
-{
-	return actoNotarialService.getActosLibros(libro);
-	
-
-}
-
-
-
-
-
+	}
 
 }
